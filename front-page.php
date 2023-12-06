0bfd502436c80ddc0a56676330ca49e695878ce4
Jinane Lotfi <jinane.lofi@gmail.com>
2023-12-06 22:21:58 +0100

    Hero (1ère section) CSS : ok

--- front-page.php
+++ front-page.php
@@ -2,36 +2,8 @@
 <main class="home-container">
 
 
-
     <!-- Section 1 -->
-    <article class="hero-container">
-        <div class="hero-container">            
-            <div class="left-talk">
-                <h2>
-                    Meet the future of your organization today
-                </h2>
-                <h1>
-                    We build software that empower public and private organizations to bridge the gap between data, decisions and strategy execution.
-                </h1>
-                <button>Let's talk</button>
-                <div class="logos-entreprises">
-                    <img class="bnp-logo" src="<?php echo get_template_directory_uri() . '/assets/images/bnp-logo.png'; ?>" alt="bnp paribas logo">
-                    <img class="thales-logo" src="<?php echo get_template_directory_uri() . '/assets/images/thales-logo.png'; ?>" alt="thales logo">
-                    <img class="axa-logo" src="<?php echo get_template_directory_uri() . '/assets/images/axa-logo.png'; ?>" alt="axa logo">
-                    <img class="republique-logo" src="<?php echo get_template_directory_uri() . '/assets/images/republique-logo.png'; ?>" alt="republique logo">
-                </div>
-            </div>
-            <div class="right-talk">
-            <img class="illustration-data" src="<?php echo get_template_directory_uri() . '/assets/images/illustration-data-management.png'; ?>" alt="illustration-data image">
-            </div>    
-        </div>
-        <div class="data-cloud">
-            <img class="data-banner" src="<?php echo get_template_directory_uri() . '/assets/images/data-banner.png'; ?>" alt="texte data cloud">
-        </div>
-        <div class="shape-divider">
-            <img class="shape-divider" src="<?php echo get_template_directory_uri() . '/assets/images/shape-divider.png'; ?>" alt="republique logo">
-        </div>
-    </article>
+    <?php get_template_part('templates/hero'); ?>
 
     <!-- Section 2 -->
     <article class="section-two-container">
@@ -140,11 +112,6 @@
         <div class="shape-divider">
             <img class="shape-divider" src="<?php echo get_template_directory_uri() . '/assets/images/shape-divider.png'; ?>" alt="republique logo">
         </div>
-
-
-
-
-
     </article>
 
 
---
+++ templates/hero.php
@@ -0,0 +1,30 @@
+<article class="hero">
+        <div class="hero-container">            
+            <div class="left-talk">
+                <h2>
+                    Meet the future<br> of your<br> <span>organization</span><br>today
+                </h2>
+                <h1>
+                    We build software that empower public and private organizations to bridge the gap between data, decisions and strategy execution.
+                </h1>
+
+                <a href="#">Let's talk</a>
+                
+                <div class="logos-entreprises">
+                    <img class="bnp-logo" src="<?php echo get_template_directory_uri() . '/assets/images/bnp-logo.png'; ?>" alt="bnp paribas logo">
+                    <img class="thales-logo" src="<?php echo get_template_directory_uri() . '/assets/images/thales-logo.png'; ?>" alt="thales logo">
+                    <img class="axa-logo" src="<?php echo get_template_directory_uri() . '/assets/images/axa-logo.png'; ?>" alt="axa logo">
+                    <img class="republique-logo" src="<?php echo get_template_directory_uri() . '/assets/images/republique-logo.png'; ?>" alt="republique logo">
+                </div>
+            </div>
+            <div class="right-talk">
+            <img class="illustration-data" src="<?php echo get_template_directory_uri() . '/assets/images/illustration-data-management.png'; ?>" alt="illustration-data image">
+            </div>    
+        </div>
+        <div class="data-cloud">
+            <img class="data-banner" src="<?php echo get_template_directory_uri() . '/assets/images/data-banner.png'; ?>" alt="texte data cloud">
+        </div>
+        <div class="shape-divider">
+            <img class="shape-divider" src="<?php echo get_template_directory_uri() . '/assets/images/shape-divider.png'; ?>" alt="republique logo">
+        </div>
+    </article>
